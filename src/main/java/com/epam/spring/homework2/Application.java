b59a8bcd805d9e843cc982701634491a09c14d62
stempzZz <kms111001@gmail.com>
2022-06-25 20:20:37 +0300

    added output of all current beans, not only bean names

--- src/main/java/com/epam/spring/homework2/Application.java
+++ src/main/java/com/epam/spring/homework2/Application.java
@@ -8,9 +8,18 @@ import java.util.Arrays;
 public class Application {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ConfigA.class);
+
         System.out.println("-------------------------");
-        Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+
+        String[] beanNames = context.getBeanDefinitionNames();
+        Arrays.stream(beanNames).forEach(System.out::println);
+
         System.out.println("-------------------------");
+
+        Arrays.stream(beanNames).forEach(name -> System.out.println(context.getBean(name)));
+
+        System.out.println("-------------------------");
+
         context.close();
     }
 }
