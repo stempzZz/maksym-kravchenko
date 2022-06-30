be2300f9cde8f22171535889721bb5d2c5299cee
stempzZz <kms111001@gmail.com>
2022-06-30 12:24:48 +0300

    commit built project by spring initializr

---
+++ src/main/java/com/epam/spring/time_tracking/TimeTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.epam.spring.time_tracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeTrackingApplication.class, args);
+	}
+
+}
